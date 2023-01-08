6eb76bb294b917cb04d1d59fc5b7526a75be57ca
Bibek Mishra <deesirouss@gmail.com>
2023-01-08 20:18:43 +0545

    deleting test ecr

--- eks/modules/stages/test/ecr.tf
+++ eks/modules/stages/test/ecr.tf
@@ -6,12 +6,3 @@ resource "aws_ecr_repository" "repository" {
   }
   tags = local.tags
 }
-
-resource "aws_ecr_repository" "test2" {
-  provider = aws.nvirginia
-  name     = "vyaguta/test2"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-  tags = local.tags
-}
